36b029e31e163455f2cb1087be5555aabcedd51a
Ryan Kurte <ryan@kurte.nz>
2020-02-11 14:06:12 +1300

    split out store errors

---
+++ src/store/error.rs
@@ -0,0 +1,45 @@
+
+use std::net::AddrParseError;
+use diesel::ConnectionError;
+use diesel::result::{Error as DieselError};
+use strum::{ParseError as StrumError};
+use base64::{DecodeError as B64Error};
+
+#[derive(Debug, PartialEq)]
+pub enum StoreError {
+    Connection(ConnectionError),
+    Diesel(DieselError),
+    Strum(StrumError),
+    B64(B64Error),
+    Addr(AddrParseError),
+}
+
+impl From<ConnectionError> for StoreError {
+    fn from(e: ConnectionError) -> Self {
+        Self::Connection(e)
+    }
+}
+
+impl From<DieselError> for StoreError {
+    fn from(e: DieselError) -> Self {
+        Self::Diesel(e)
+    }
+}
+
+impl From<StrumError> for StoreError {
+    fn from(e: StrumError) -> Self {
+        Self::Strum(e)
+    }
+}
+
+impl From<B64Error> for StoreError {
+    fn from(e: B64Error) -> Self {
+        Self::B64(e)
+    }
+}
+
+impl From<AddrParseError> for StoreError {
+    fn from(e: AddrParseError) -> Self {
+        Self::Addr(e)
+    }
+}
